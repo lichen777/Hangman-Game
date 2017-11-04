ec202cc34bb3afdac949910085c65a040aeb16a5
Lichen <LC@Lichens-MacBook-Air.local>
2017-11-04 15:30:09 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,83 @@
 // pick theme
 var currentWordsGroup = [];
 
-var theme1Words = ["apple", "peach", "pear"];
-var theme2Words = ["cat", "dog", "horse"];
-var theme3Words = ["intel", "oracle", "facebook", "google"];
+var theme1 = {
+    name: "theme1",
+    words: ["intel", "oracle", "facebook", "google"],
+    styleLink: "assets/css/theme1.css",
+    imageLink: "assets/images/theme1.png"
+  };
 
+var  theme2 = {
+    name: "theme2",
+    words: ["cat", "dog", "horse"],
+    styleLink: "assets/css/theme2.css",
+    imageLink: "assets/images/theme2.png"
+  };
+var  theme3 = {
+    name: "theme3",
+    words: ["apple", "peach", "pear"],
+    styleLink: "assets/css/theme3.css",
+    imageLink: "assets/images/theme3.png"
+  };
 
-function pickTheme1() {
-    document.querySelector("#theme").innerHTML = "Your selected theme: Theme1";
-    document.getElementById("theme_css").href = "assets/css/theme1.css";
-  //document.getElementById("theme_img").src = "assets/images/theme1.png";
-    currentWordsGroup = theme1Words;
-}
-
-function pickTheme2() {
-    document.querySelector("#theme").innerHTML = "Your selected theme: Theme2";
-  document.getElementById("theme_css").href = "assets/css/theme2.css";
-  //document.getElementById("theme_img").src = "assets/images/theme2.png";
-    currentWordsGroup = theme2Words;
-
-}
-
-function pickTheme3() {
-    document.querySelector("#theme").innerHTML = "Your selected theme: Theme3";
-    document.getElementById("theme_css").href = "assets/css/theme3.css";
-    //document.getElementById("theme_img").src = "assets/images/theme3.png";
-    currentWordsGroup = theme3Words;
-
+function pickTheme(theme) {
+    document.querySelector("#theme").innerHTML = "Your selected theme: " + theme.name;
+    document.getElementById("theme_css").href = theme.styleLink;
+    document.getElementById("theme_img").src = theme.imageLink;
+    currentWordsGroup = theme.words;
 }
 
 var wins = 0;
 var lives = 5;
 var alreadyGuessed = [];
+var wordToGuess;
 
-document.onkeyup = function(event) {
+function arrayToString(a) {
+  return a.toString();
+}
 
-  document.getElementById("game").style.display = "inline";
+function stringToArray(a) {
+  return a.split("");
+}
+
+function startGame() {
+  document.getElementById("input").style.display = "inline";
 
   document.getElementById("start").style.display = "none";
 
-
-  var wordToGuess = currentWordsGroup[Math.floor(Math.random() * currentWordsGroup.length)];
+  wordToGuess = currentWordsGroup[Math.floor(Math.random() * currentWordsGroup.length)];
   console.log (wordToGuess);
 
   var blank = "";
 
   for (var i = 0; i < wordToGuess.length; i++) {
-    blank += " _";
+    blank += "_";
   }
+
+  var arrayWord = wordToGuess.split("");
+
+  var arrayBlank = stringToArray(blank);
+
+  // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+  var html =
+    "<br><p>wins: " + wins + "</p>" +
+    "<p>Lives: " + lives + "</p>" + 
+    "<p>Word to guess: </p>" + "<p id='blank'>" + blank + "</p>" +
+    "<p>Letter already Guessed: " + alreadyGuessed + "</p>";
+        
+
+  // Set the inner HTML contents of the #game div to our html string
+  document.querySelector("#game").innerHTML = html;
+
 }
+
+
 /*
+//document.onkeyup = function(event) {
+
   var userGuess = event.key;
   console.log (userGuess);
 
-
-  // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-    var html =
-        "<p>wins: " + wins + "</p>" +
-        "<p>Lives: " + lives + "</p>" + 
-        "<p>Word to guess: </p>" + "<p>" + blank + "</p>" +
-        "<p>Letter already Guessed: " + alreadyGuessed + "</p>";
-        
-
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#game").innerHTML = html;
-
-}
 */
 //
